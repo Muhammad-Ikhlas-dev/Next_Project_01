ea1128e8b4d5fc599eadc24735cc6ac4c1fc397f
MUHAMMAD IKHLAS <mikhlas78690@gmail.com>
2024-10-02 14:42:40 +0500

    changes in file to be merged in main branch

--- src/app/to_merge.js
+++ src/app/to_merge.js
@@ -1,6 +1,2 @@
 console.log('this file i will merge in main')
 console.log('this file i will merge in main')
-console.log('this file i will merge in main')
-console.log('this file i will merge in main')
-console.log('this file i will merge in main')
-console.log('this file i will merge in main')
